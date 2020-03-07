d5f46f620670164669a46ef6264074436affc439
Durosinmi Olorunfemi <dee_femifose@yahoo.com>
2020-03-07 00:55:43 -0800

    changes

--- app/src/main/java/com/femidof/unobliviate/CardActivity.java
+++ app/src/main/java/com/femidof/unobliviate/CardActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class CardActivity extends AppCompatActivity {
@@ -12,12 +13,31 @@ public class CardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card);
+
+        findViewById(R.id.ic_close).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+        findViewById(R.id.ic_save).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent data = new Intent(); // create a new Intent, this is where we will put our data
+                data.putExtra("string1", ((EditText) findViewById(R.id.editTextField)).getText().toString()); // puts one string into the Intent, with the key as 'string1'
+                data.putExtra("string2", ((EditText) findViewById(R.id.answerTextField)).getText().toString()); // puts another string into the Intent, with the key as 'string2
+                setResult(RESULT_OK, data); // set result code and bundle data for response
+                finish(); // closes this activity and pass data to the original activity that launched this activity
+            }
+        });
+
+
+
+
     }
 
-    (ImageView) findViewById(R.id.ic_close).setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            finish();
-        }
-    });
+
+
 }
--- app/src/main/java/com/femidof/unobliviate/MainActivity.java
+++ app/src/main/java/com/femidof/unobliviate/MainActivity.java
@@ -3,7 +3,9 @@ import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,18 +14,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView question = (TextView) findViewById(R.id.question);
+
+        question.setText("Who is the 44th president of the Uniteed State of America");
+
+        question.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Button","Hello");
+                findViewById(R.id.question).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+            }
+        });
 
         findViewById(R.id.ic_card_card).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, CardActivity.class);
-                MainActivity.this.startActivity(intent);
+                MainActivity.this.startActivityForResult(intent,100);
             }
         });
+
+        TextView answer = (TextView) findViewById(R.id.flashcard_answer);
+        answer.setText("Obama");
+        answer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.question).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+            }
+        });
+
+
+
+
+
     }
-//openCardActivity
-//    public void openCardActivity() {
-//        Intent intent = new Intent(this, card)
-//    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) { // this 100 needs to match the 100 we used when we called startActivityForResult!
+            String string1 = data.getExtras().getString("string1"); // 'string1' needs to match the key we used when we put the string in the Intent
+            String string2 = data.getExtras().getString("string2");
+
+            TextView question = (TextView) findViewById(R.id.question);
+            TextView answer = (TextView) findViewById(R.id.flashcard_answer);
+
+            question.setText(string1);
+            answer.setText(string2);
+
+        }
+    }
 
 }
